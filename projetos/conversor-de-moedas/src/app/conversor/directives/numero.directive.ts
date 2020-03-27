d0029946132f5f204ab90f04efab74c2400f4d6f
Caio Dantas <caioaires@gmail.com>
2020-03-27 18:45:09 -0300

    até a aula 106

---
+++ projetos/conversor-de-moedas/src/app/conversor/directives/numero.directive.ts
@@ -0,0 +1,23 @@
+import { Directive, HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[numero]'
+})
+export class NumeroDirective {
+
+  constructor() { }
+  @HostListener('keyup', ['$event'])
+  onKeyUp($event: any) {
+  	let valor = $event.target.value;
+  	let posDecimais = valor.indexOf('.');
+
+  	valor = valor.replace(/[\D]/g,'');
+
+  	if(posDecimais > 0) {
+  		valor = valor.substr(0, posDecimais) + '.'
+  			+ valor.substr(posDecimais)
+  	}
+
+  	$event.target.value = valor;
+  }
+}
